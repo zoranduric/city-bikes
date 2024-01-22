969f922389656c84b61caf7edf065e43afc25cfa
Zoran <zordjuric@gmail.com>
2024-01-22 10:36:38 +0200

    Initialization of Express.js with TypeScript, ESLint and Prettier configuration.

---
+++ backend/src/main.ts
@@ -0,0 +1,22 @@
+import morgan from 'morgan';
+import cors from 'cors';
+import express, { Application, Response } from 'express';
+
+const app: Application = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+
+app.get('/', (_req, res: Response) => {
+  res.json({
+    message: 'Sever is running.',
+  });
+});
+
+export const server = app.listen(5000, () => {
+  console.log('Server listening on port 5000 ');
+});
+
+export default app;
