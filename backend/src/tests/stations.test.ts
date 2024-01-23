1cf8c0daa43945c7c732456cb1d00d0a918a3800
Zoran <zordjuric@gmail.com>
2024-01-23 10:18:04 +0200

    Add tests for getStations route to verify it returns an array of stations and a 400 status when incorrect query parameters are provided

--- backend/src/tests/stations.test.ts
+++ backend/src/tests/stations.test.ts
@@ -1,6 +1,7 @@
 import { afterAll, describe, expect, test } from 'vitest';
 import supertest from 'supertest';
 import app, { server } from '../main';
+import { Stations } from '../stations/schema';
 
 describe('Station tests', () => {
   afterAll(() => {
@@ -10,5 +11,15 @@ describe('Station tests', () => {
     const response = await supertest(app).get('/stations/count');
     expect(response.status).toBe(200);
     expect(parseInt(response.text)).toBeGreaterThan(0);
+  });
+  test('Fetch a list of stations and ensure it contains at least one station', async () => {
+    const response = await supertest(app).get('/stations').query({ skip: 0, take: 10 });
+    expect(response.status).toBe(200);
+    const responseBody = response.body as Stations[];
+    expect(responseBody.length).toBeGreaterThan(0);
+  });
+  test('Return an error when incorrect query parameters are provided', async () => {
+    const response = await supertest(app).get('/stations').query({ skip: 0, take: 11 });
+    expect(response.status).toBe(400);
   });
 });
